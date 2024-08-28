52d3d391d191842ec483f1ee467c7f66208b069e
Kamaleeswari-S <princykamalikumar@gmail.com>
2024-08-28 13:12:10 +0530

    React Redux Task-32

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+ import Navbar from "./Components/Navbar";
+ import Footer from "./Components/Footer";
+ import { Provider } from "react-redux";
+ import { productStore } from "./Utils/features.jsx/ProductsStore";
+ import Home from "./Pages/Home";
+ import "bootstrap/dist/css/bootstrap.min.css";
+
+
+ const App = () => {
+   return (
+     <div>
+       <Provider store={productStore}>
+         <Navbar />
+         <Home />
+       </Provider>
+
+       <Footer />
+     </div>
+   );
+ };
+
+ export default App;
